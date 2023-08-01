787e120b7ae4406fe820dc33f440adea09d671fb
Dumindu Jayarathne <dumij58@gmail.com>
2023-08-01 07:19:29 +0530

    Add files I forgot to add in the last commit

---
+++ helpers.py
@@ -0,0 +1,41 @@
+import functools
+
+from flask import redirect, url_for, render_template, g, session as flask_session
+from .models import Admin
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+def admin_only(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if not Admin.query.filter(Admin.username == g.user.username).first():
+            return render_template('error.html', e_code = 403, e_text = "Access Denied")
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+def f_phone_no(number):
+    return f"+{str(int(number))}"
+
+def f_gender(gender):
+    if gender == "f":
+        return "Female"
+    if gender == "m":
+        return "Male"
+    
+def f_datetime(dt):
+    return '{:%Y-%m-%d %H:%M:%S}'.format(dt)
+
+def f_date(d):
+    return '{:%Y-%m-%d}'.format(d)
